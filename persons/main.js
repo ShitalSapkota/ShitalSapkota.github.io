9a9d72b0fcaffa4220053fc3c9bef336e4e3a4e2
ShitalSapkota <shital.sapkota1@gmail.com>
2024-10-23 15:50:46 +0300

    add cat avatar

--- persons/main.js
+++ persons/main.js
@@ -1,10 +1,10 @@
 const cardcontainer = document.querySelector('.container');
 
-function createCard(robot){
+function addCard(robot){
   const card = document.createElement("div");
   card.classList.add("card");
   const image = document.createElement("img");
-  image.src = `https://robohash.org/${robot.id}`;
+  image.src = `https://robohash.org/${robot.id}?set=set4`;
   card.appendChild(image);
   const cardText = document.createElement("div");
   cardText.innerHTML = `<p><b>Name:</b> ${robot.name}</p>
@@ -20,9 +20,8 @@ async function fetchPerson() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json()
   //console.log(data)
-  data.forEach(robot => createCard(robot))
+  data.forEach(robot => addCard(robot))
 }
 fetchPerson();
 
-// persons project is for displaying individual data of the user
-// In this case User is a 🤖 Robot
+// persons project is for displaying individual data of the user using cat images as a avatar
